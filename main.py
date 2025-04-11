635d4278308872b83447391e6a4dc8e5a04d7161
912-Dumitrana-Mihnea <mihnea.dumitrana@gmail.com>
2025-04-11 20:10:45 +0300

    LIC-9: enable all benchmarks

--- main.py
+++ main.py
@@ -16,25 +16,25 @@ if __name__ == "__main__":
     print(f"[System Info]\nCPU: {get_cpu_info()}\nGPU: {get_gpu_info()}")
     hasGPU = get_gpu_info() != "No GPU detected"
 
-    # print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
-    # print("Running Matrix multiplication tests...")
-    # run_matrix_mult_cpu_single_threaded_prof(RUNS, MATRIX_SIZE)
-    # run_matrix_mult_cpu_multi_threaded_prof(RUNS, MATRIX_SIZE)
-    # run_matrix_mult_gpu_acceleration_prof(RUNS, MATRIX_SIZE) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
-    #
-    # # same but 10x matrix size
-    # print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
-    # print("Running BIG Matrix multiplication tests...")
-    # run_matrix_mult_cpu_single_threaded_prof(RUNS, MATRIX_SIZE*10)
-    # run_matrix_mult_cpu_multi_threaded_prof(RUNS, MATRIX_SIZE*10)
-    # run_matrix_mult_gpu_acceleration_prof(RUNS, MATRIX_SIZE*10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
-    #
-    # print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
-    # print("Running SMALL Radix Sort tests...")
-    # run_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH//10)
-    # run_radix_sort_multi_threaded_prof(RUNS, ARRAY_LENGTH//10)
-    # run_optimised_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH//10)
-    # # run_radix_sort_gpu_acceleration_prof(RUNS, ARRAY_LENGTH//10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
+    print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
+    print("Running Matrix multiplication tests...")
+    run_matrix_mult_cpu_single_threaded_prof(RUNS, MATRIX_SIZE)
+    run_matrix_mult_cpu_multi_threaded_prof(RUNS, MATRIX_SIZE)
+    run_matrix_mult_gpu_acceleration_prof(RUNS, MATRIX_SIZE) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
+
+    # same but 10x matrix size
+    print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
+    print("Running BIG Matrix multiplication tests...")
+    run_matrix_mult_cpu_single_threaded_prof(RUNS, MATRIX_SIZE*10)
+    run_matrix_mult_cpu_multi_threaded_prof(RUNS, MATRIX_SIZE*10)
+    run_matrix_mult_gpu_acceleration_prof(RUNS, MATRIX_SIZE*10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
+
+    print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
+    print("Running SMALL Radix Sort tests...")
+    run_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH//10)
+    run_radix_sort_multi_threaded_prof(RUNS, ARRAY_LENGTH//10)
+    run_optimised_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH//10)
+    # run_radix_sort_gpu_acceleration_prof(RUNS, ARRAY_LENGTH//10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
 
     print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
     print("Running Radix Sort tests...")
@@ -43,12 +43,12 @@ if __name__ == "__main__":
     run_optimised_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH)
     # run_radix_sort_gpu_acceleration_prof(RUNS, ARRAY_LENGTH) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
 
-    # # same but 10x array length
-    # print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
-    # print("Running BIG Radix Sort tests...")
-    # run_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH*10)
-    # run_radix_sort_multi_threaded_prof(RUNS, ARRAY_LENGTH*10)
-    # run_optimised_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH*10)
-    # # run_radix_sort_gpu_acceleration_prof(RUNS, ARRAY_LENGTH*10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
+    # same but 10x array length
+    print(f"\nElapsed time: {get_formatted_elapsed_time(startTime)}")
+    print("Running BIG Radix Sort tests...")
+    run_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH*10)
+    run_radix_sort_multi_threaded_prof(RUNS, ARRAY_LENGTH*10)
+    run_optimised_radix_sort_single_threaded_prof(RUNS, ARRAY_LENGTH*10)
+    # run_radix_sort_gpu_acceleration_prof(RUNS, ARRAY_LENGTH*10) if hasGPU else print("No CUDA enabled GPU detected, skipping GPU test")
 
     print("No automatic publishing of results yet, just send me the results folder as zip and I'll handle the ranking ;)")
